8eaf6ff23c4546f37b0469fbffba68408115d85b
DF1229 <daanfaber1@gmail.com>
2022-01-31 11:29:06 +0100

    Added main menu

--- Assets/Scripts/MainMenu/MenuManager.cs
+++ Assets/Scripts/MainMenu/MenuManager.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject howToMenu;
+
+    public void PlayGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleHowToMenu()
     {
-        
+        if (howToMenu.activeSelf)
+            howToMenu.SetActive(true);
+        else
+            howToMenu.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
